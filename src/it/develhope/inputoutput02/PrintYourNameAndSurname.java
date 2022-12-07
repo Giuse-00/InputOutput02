882502f8c8301780ce13eafc86740f673dc547f8
Giuseppe Iacono <giuseiacono10@gmail.com>
2022-12-07 17:48:50 +0100

    Iacono esercizio input and output 02

---
+++ src/it/develhope/inputoutput02/PrintYourNameAndSurname.java
@@ -0,0 +1,10 @@
+package it.develhope.inputoutput02;
+
+public class PrintYourNameAndSurname {
+
+    public static void main(String[] args) {
+
+        System.out.print("Giuseppe ");
+        System.out.println("Iacono");
+    }
+}
